819b12cbfd61df2991891bb37b814284c449a318
Rohan Kumar <rohaan@redhat.com>
2021-07-05 20:44:22 +0530

    Remove artifactSets

--- src/main/java/io/fabric8/demo/EventExample.java
+++ src/main/java/io/fabric8/demo/EventExample.java
@@ -1,12 +1,18 @@
 package io.fabric8.demo;
 
+import io.fabric8.kubernetes.api.model.Event;
+import io.fabric8.kubernetes.api.model.ObjectMeta;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
 
 public class EventExample {
     public static void main(String[] args) {
         try (KubernetesClient client = new DefaultKubernetesClient()) {
-            client.v1().events().inNamespace("default").list().getItems();
+            client.v1().events().inNamespace("default").list().getItems()
+                    .stream()
+                    .map(Event::getMetadata)
+                    .map(ObjectMeta::getName)
+                    .forEach(System.out::println);
         }
     }
 }
